f34b02150d7ea11a39af14d0b908dc095b3cba71
Titas <tlasic16@student.aau.dk>
2022-06-02 19:40:19 +0200

    separated pressure and flow, ignoring wav files

--- AcousticTube.py
+++ AcousticTube.py
@@ -13,52 +13,58 @@ set_matplotlib_formats('svg')
 
 import numpy as np
 import matplotlib.pyplot as plt
+# %matplotlib qt  # makes plots in a separate window
 import math
 from pylab import *
 from drawnow import figure, drawnow
 plt.close('all')
+import scipy
+from scipy import interpolate
 
 
+EXCITER = 0 # set 0 for flow, 1 for pressure
 
 #%%  Definitions
 
-fs = 44100
-k = 1/fs
-B = 142000;             # bulk modulus of the air
-rho = 1.225;            # air density
-c = math.sqrt(B/rho);
-h = c*k;                # calculate grid spacing
-L = 1;                  # scaling lenght
-gamma = c/L;            # scaling
-durration = 1;          # synthesised sound lenght in s
-dur = fs*durration;
+fs = 44100.
+k = 1./fs
+B = 142000.              # bulk modulus of the air
+rho = 1.225             # air density
+c = math.sqrt(B/rho) 
+h = c*k                 # calculate grid spacing
+L = 1                   # scaling lenght
+gamma = c/L             # scaling
+durration = 1.           # synthesised sound lenght in s
+dur = int(math.floor(fs*durration))
 
-N = math.floor(1/h);
-h = 1/N;
-lambdaSq = c**2 * k**2 / h**2; #sanity check
-# lambda = gamma * k /h;
-
-
-# intialise states of the system
-
-PsiNext = np.zeros(N);
-Psi = np.zeros(N);
-PsiPrev = np.zeros(N);
+N = math.floor(1/h) 
+h = 1/N 
+lambdaSq = c**2 * k**2 / h**2  #sanity check
+# lambda = gamma * k /h 
 
 # intialise states of the system
 
-PsiNext = np.zeros(N);
-Psi = np.zeros(N);
-PsiPrev = np.zeros(N);
+PsiNext = np.zeros(N) 
+Psi = np.zeros(N) 
+PsiPrev = np.zeros(N) 
 
 
 # initialise output
-out = np.zeros(dur);
+out = np.zeros(dur) 
 
-rangeI = np.arange(1,N-1) #range for inner update equation "l"
-S = np.ones(N);
-M = len(S);
-rangeS = np.arange(1,N-1); #range of Shape "S" for inner update equation "S_l"
+# Create mock shape function
+shapeSampled = np.ones(10)
+ 
+# Interpolate shape function to match the tube in array length
+
+M = len(shapeSampled)
+x = np.arange(0,M,1) 
+f = interpolate.interp1d(x, shapeSampled)
+
+xnew = np.arange(0, 1, h)
+S = f(xnew) # shape function is from now just "S"
+#%%
+
 avgS = np.zeros(N)
 
 i=0
@@ -67,60 +73,106 @@ for i in range(N-1):
 avgS[0] = S[0] # just made them to work, its not correct though
 avgS[N-1] = S[N-1] # just made them to work, its not correct though
 
-a1 = 1/(2*(0.8216**2) * gamma); # page 253 bilbao
-a2 = L/(0.8216 * math.sqrt(avgS[0]*S[0]/math.pi));
+a1 = 1/(2*(0.8216**2) * gamma)  # page 253 bilbao
+a2 = L/(0.8216 * math.sqrt(avgS[0]*S[0]/math.pi))
 
-exciter = np.zeros(dur);
-# exciter[0] = 1;
-f0 = 200;
+exciterP = np.zeros(dur)
+exciterV = np.zeros(dur)
+# exciter[0] = 1 
+f0 = 200 
 # t = np.linspace(0.,dur/(f0/200.))
 t = 0
+
+    
 for t in range(int(dur/(f0/10))):
-    exciter[t] = math.cos(2*math.pi*f0*(t/fs));
+    exciterV[t] =  math.cos(2*math.pi*f0*(t/fs))
 
 
-#%% Loop
 def draw_fig():
     plt.ylim(-0.5,0.5)
     plt.plot(PsiNext)
     
-
-n = 0
-for n in range(dur):
-    l=1
-    for l in range(N-1):    
-        PsiNext[l] = 2*(1-lambdaSq)*Psi[l] - PsiPrev[l] + \
-            lambdaSq * (S[l+1]/avgS[l]) * Psi[l+1] + \
-            lambdaSq * (S[l]/avgS[l]) * Psi[l-1]
-            
-          
-    # flow as an input
-    PsiNext[0] = 2*lambdaSq*Psi[1] + 2*(1-lambdaSq)*Psi[0] - PsiPrev[0] + (2*h*lambdaSq*S[0]/avgS[0])*exciter[n];
-    
-    # pressure as an input
-    # PsiNext[0] = (1-2*lambdaSq)*Psi[0] - (k/rho)*exciter[n] + \
-    #     lambdaSq * (S[1]/avgS[0]) * Psi[1] + \
-    #     lambdaSq * (S[0]/avgS[0] * Psi[0])
-    
-    num = 2*(1-lambdaSq)*Psi[N-1] - PsiPrev[N-1] + (h*((a1/k) - a2)*(lambdaSq*S[N-1])/avgS[N-1])*PsiPrev[N-1] + 2*lambdaSq*Psi[N-2]; 
-    den = 1 + h*((a1/k) + a2)*(lambdaSq*S[N-1]/avgS[N-1]);
-    PsiNext[N-1] = num/den;
+#%% Flow input  
+if EXCITER==0:
+    print("flow")
+    for t in range(int(dur/(f0/10))):
+        exciterV[t] =  math.cos(2*math.pi*f0*(t/fs))# input is a cos wave
         
-
-    # drawnow(draw_fig)
+    n = 0
+    # Loop for flow input
+    for n in range(dur):
+        l=1
+        for l in range(N-1):    
+            PsiNext[l] = 2*(1-lambdaSq)*Psi[l] - PsiPrev[l] + \
+                lambdaSq * (S[l+1]/avgS[l]) * Psi[l+1] + \
+                lambdaSq * (S[l]/avgS[l]) * Psi[l-1]
+                
+              
+        # flow as an input
+        PsiNext[0] = 2*lambdaSq*Psi[1] + 2*(1-lambdaSq)*Psi[0] - PsiPrev[0] + (2*h*lambdaSq*S[0]/avgS[0])*exciterV[n] 
+        
+        num = 2*(1-lambdaSq)*Psi[N-1] - PsiPrev[N-1] + (h*((a1/k) - a2)*(lambdaSq*S[N-1])/avgS[N-1])*PsiPrev[N-1] + 2*lambdaSq*Psi[N-2]  
+        den = 1 + h*((a1/k) + a2)*(lambdaSq*S[N-1]/avgS[N-1]) 
+        PsiNext[N-1] = num/den 
+            
     
-    out[n] = PsiNext[N-1];
-    PsiPrev  = Psi.copy();
-    Psi = PsiNext.copy();  
+        # drawnow(draw_fig)
+        
+        # out[n] = PsiNext[N-1]
+        
+        # output as flow
+        out[n] = (Psi[N-3] + Psi[N-1])/(2*h)
+        
+        PsiPrev  = Psi.copy() 
+        Psi = PsiNext.copy()   
+
+ 
+#%% Pressure input
+if EXCITER==1:
+    print("pressure")
+    for t in range(int(dur/(f0/10))):
+        exciterP[t] = 2000 * math.cos(2*math.pi*f0*(t/fs)) # input is a cos wave
+        
+    n = 0    
+    # Loop for pressure input
+    
+    for n in range(dur):
+        l=1
+        for l in range(N-1):    
+            PsiNext[l] = 2*(1-lambdaSq)*Psi[l] - PsiPrev[l] + \
+                lambdaSq * (S[l+1]/avgS[l]) * Psi[l+1] + \
+                lambdaSq * (S[l]/avgS[l]) * Psi[l-1]
+                
+              
+            
+        # pressure as an input
+        PsiNext[0] = PsiPrev[0].copy() - ((2.*k)/rho)*exciterP[n]
+        
+        num = 2*(1-lambdaSq)*Psi[N-1] - PsiPrev[N-1] + (h*((a1/k) - a2)*(lambdaSq*S[N-1])/avgS[N-1])*PsiPrev[N-1] + 2*lambdaSq*Psi[N-2]  
+        den = 1 + h*((a1/k) + a2)*(lambdaSq*S[N-1]/avgS[N-1]) 
+        PsiNext[N-1] = num/den 
+            
+    
+        # drawnow(draw_fig)
+        
+        # out[n] = PsiNext[N-1]
+        
+        # output as pressure
+        out[n] = rho*(PsiNext[N-1]-PsiPrev[N-1])/(2*k)
+        
+        
+        PsiPrev  = Psi.copy() 
+        Psi = PsiNext.copy()   
     
 
 #%% Plot
 
-plt.plot(exciter)
+# plt.plot(exciter)
+
 plt.plot(out)
 
-#%% Audio
+#%% Save array as an audio file
 from scipy.io.wavfile import write
 
 scaled = np.int16(out/np.max(np.abs(out)) * 32767)
-write('test.wav', 44100, scaled)
+write('test.wav', int(fs), scaled)
